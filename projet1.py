4570f802baeb5a734944ff2f2207c64223fd94c1
latyr <fayelatyr92@gmail.com>
2023-03-06 08:21:23 +0000

    modif

--- projet1.py
+++ projet1.py
@@ -18,91 +18,91 @@ with open(fichier, 'r') as monlecteur:
 Calcul_moyenne(fichier)
 
 
-# def valider_notes(note):
-#     note= note.split("#")
-#     #verifier que chaque note est format[devoir1|devoir2:examen]
-#     for i in note.split('#'):
-#         if not re.match(r'^[A-Za-z]+\[\d+\|\d+:\d+\]$', note):
-#             return False
-#     return  
+def valider_notes(note):
+    note= note.split("#")
+    #verifier que chaque note est format[devoir1|devoir2:examen]
+    for i in note.split('#'):
+        if not re.match(r'^[A-Za-z]+\[\d+\|\d+:\d+\]$', note):
+            return False
+    return  
 
-# # Définition des expressions régulières pour les vérifications des
-# NUMERO_REGEX = r'^[A-Z0-9]{7}$'
-# NOM_REGEX = r'^[a-zA-Z]{2,}$'
-# PRENOM_REGEX = r'^[a-zA-Z]{3,}$'
-# DATE_REGEX = r'^\d{2}/\d{2}/\d{4}$'
-# CLASSE_REGEX = r'^[3456]eme?[A-Z]$'
-# NOTE_REGEX = r'^\d{1,2}(\|\d{1,2}:\d{2})?$'
+# Définition des expressions régulières pour les vérifications des
+NUMERO_REGEX = r'^[A-Z0-9]{7}$'
+NOM_REGEX = r'^[a-zA-Z]{2,}$'
+PRENOM_REGEX = r'^[a-zA-Z]{3,}$'
+DATE_REGEX = r'^\d{2}/\d{2}/\d{4}$'
+CLASSE_REGEX = r'^[3456]eme?[A-Z]$'
+NOTE_REGEX = r'^\d{1,2}(\|\d{1,2}:\d{2})?$'
 
-# Tableaux pour les données valides et invalides
-# donnees_valides = []
-# donnees_invalides = []
+#Tableaux pour les données valides et invalides
+donnees_valides = []
+donnees_invalides = []
 
-# # Ouverture du fichier contenant les données des élèves
-# with open('/home/faye/Documents/Donnees_Projet_Python_DataC5.csv', 'r') as monlecteur:
-#     monlecteur_csv = csv.reader(monlecteur)
+# Ouverture du fichier contenant les données des élèves
+with open('/home/faye/Documents/Donnees_Projet_Python_DataC5.csv', 'r') as monlecteur:
+    monlecteur_csv = csv.reader(monlecteur)
 
-#     # Lecture des lignes du fichier
-#     for i  in monlecteur_csv:
+    # Lecture des lignes du fichier
+    for i  in monlecteur_csv:
         
-#         # print(i)
+        # print(i)
         
-#         #Suppression des espaces en début et fin de ligne
-#         #line = line.strip()
+        #Suppression des espaces en début et fin de ligne
+        #line = line.strip()
         
-#         # Séparation des données de la ligne
-#         #monlecteur_csv= line.split(',')
+        # Séparation des données de la ligne
+        #monlecteur_csv= line.split(',')
         
-#         #Vérification de la validité des données de la ligne
-#         numero_valide = bool(re.match(NUMERO_REGEX, i[1]))
-#         nom_valide = bool(re.match(NOM_REGEX, i[2]))
-#         prenom_valide = bool(re.match(PRENOM_REGEX, i[3]))
-#         date_valide = bool(re.match(DATE_REGEX, i[4]))
-#         classe_valide = bool(re.match(CLASSE_REGEX, i[5]))
+        #Vérification de la validité des données de la ligne
+        numero_valide = bool(re.match(NUMERO_REGEX, i[1]))
+        nom_valide = bool(re.match(NOM_REGEX, i[2]))
+        prenom_valide = bool(re.match(PRENOM_REGEX, i[3]))
+        date_valide = bool(re.match(DATE_REGEX, i[4]))
+        classe_valide = bool(re.match(CLASSE_REGEX, i[5]))
         
-#         # Vérification de la validité des notes
-#         note_valide = True
-#         moyenne = 0.0
-#         for note in monlecteur_csv:
+        # Vérification de la validité des notes
+        note_valide = True
+        moyenne = 0.0
+        for note in monlecteur_csv:
             
-#             note_valide = bool(re.match(NOTE_REGEX, note))
-#             if not note_valide:
-#                 note_valide = False
-#                 break
-#             else:
-#                 note_devoir, note_examen = note.split('|') if '|' in note else (note, '0')
-#                 note_devoir = float(note_devoir)
-#                 note_examen = float(note_examen.split(':')[0])
-#         moyenne += (note_devoir + 2 * note_examen) / 3
+            note_valide = bool(re.match(NOTE_REGEX, note))
+            if not note_valide:
+                note_valide = False
+                break
+            else:
+                note_devoir, note_examen = note.split('|') if '|' in note else (note, '0')
+                note_devoir = float(note_devoir)
+                note_examen = float(note_examen.split(':')[0])
+        moyenne += (note_devoir + 2 * note_examen) / 3
         
-#         # Si toutes les données sont valides, on ajoute la ligne avec la moyenne au tableau des données valides
-#         if numero_valide and nom_valide and prenom_valide and date_valide and classe_valide:
-#             classe = i[5].replace('eme ', 'eme').replace('ieme ', 'ieme')
-#             donnees_valides.append({
-#                 'code': i[0],
-#                 'numero': i[1],
-#                 'nom': i[2],
-#                 'prenom': i[3],
-#                 'date_naissance': i[4],
-#                 'classe': classe,
-#                 'notes': note_examen,
-#                 'notes' : note_examen,
-#                 'moyenne': moyenne / len(i[6:])
-#             })
+        # Si toutes les données sont valides, on ajoute la ligne avec la moyenne au tableau des données valides
+        if numero_valide and nom_valide and prenom_valide and date_valide and classe_valide:
+            classe = i[5].replace('eme ', 'eme').replace('ieme ', 'ieme')
+            donnees_valides.append({
+                'code': i[0],
+                'numero': i[1],
+                'nom': i[2],
+                'prenom': i[3],
+                'date_naissance': i[4],
+                'classe': classe,
+                'notes': note_examen,
+                'notes' : note_examen,
+                'moyenne': moyenne / len(i[6:])
+            })
         
-#         # Sinon, on ajoute la ligne au tableau des données invalides
-#         else:
-#             donnees_invalides.append(i)
+        # Sinon, on ajoute la ligne au tableau des données invalides
+        else:
+            donnees_invalides.append(i)
         
 
-# # Menu pour l'affichage des données
-#         while True:
-#             choix = input('Que voulez-vous afficher ? (1: données valides, 2: données invalides, 3: quitter) ')
-#             if choix == '1':
-#                 for eleve in donnees_valides:
-#                     print(donnees_valides)
-#             else:
-#                 print(donnees_valides)
-#     for i in monlecteur_csv:
-#         f=valider_notes(i[6])
-#         print(f)
+# Menu pour l'affichage des données
+        while True:
+            choix = input('Que voulez-vous afficher ? (1: données valides, 2: données invalides, 3: quitter) ')
+            if choix == '1':
+                for eleve in donnees_valides:
+                    print(donnees_valides)
+            else:
+                print(donnees_valides)
+    for i in monlecteur_csv:
+        f=valider_notes(i[6])
+        print(f)
